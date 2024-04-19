2cecacddf6cf53fc62c8bad615f12a83234ce428
Santo cyriac <132189839+Tw0side@users.noreply.github.com>
2024-04-19 15:09:42 +0530

    Update Roundrobin.c

--- Roundrobin.c
+++ Roundrobin.c
@@ -1,68 +1,66 @@
-#include<stdio.h>  
-#include<conio.h>  
-  
-void main()  
-{  
-    // initlialize the variable name  
-    int i, NOP, sum=0,count=0, y, quant, wt=0, tat=0, at[10], bt[10], temp[10];  
-    float avg_wt, avg_tat;  
-    printf(" Total number of process in the system: ");  
-    scanf("%d", &NOP);  
-    y = NOP; // Assign the number of process to variable y  
-  
-// Use for loop to enter the details of the process like Arrival time and the Burst Time  
-for(i=0; i<NOP; i++)  
-{  
-printf("\n Enter the Arrival and Burst time of the Process[%d]\n", i+1);  
-printf(" Arrival time is: \t");  // Accept arrival time  
-scanf("%d", &at[i]);  
-printf(" \nBurst time is: \t"); // Accept the Burst time  
-scanf("%d", &bt[i]);  
-temp[i] = bt[i]; // store the burst time in temp array  
-}  
-// Accept the Time qunat  
-printf("Enter the Time Quantum for the process: \t");  
-scanf("%d", &quant);  
-// Display the process No, burst time, Turn Around Time and the waiting time  
-printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");  
-for(sum=0, i = 0; y!=0; )  
-{  
-if(temp[i] <= quant && temp[i] > 0) // define the conditions   
-{  
-    sum = sum + temp[i];  
-    temp[i] = 0;  
-    count=1;  
-    }     
-    else if(temp[i] > 0)  
-    {  
-        temp[i] = temp[i] - quant;  
-        sum = sum + quant;    
-    }  
-    if(temp[i]==0 && count==1)  
-    {  
-        y--; //decrement the process no.  
-        printf("\nProcess No[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i+1, bt[i], sum-at[i], sum-at[i]-bt[i]);  
-        wt = wt+sum-at[i]-bt[i];  
-        tat = tat+sum-at[i];  
-        count =0;     
-    }  
-    if(i==NOP-1)  
-    {  
-        i=0;  
-    }  
-    else if(at[i+1]<=sum)  
-    {  
-        i++;  
-    }  
-    else  
-    {  
-        i=0;  
-    }  
-}  
-// represents the average waiting time and Turn Around time  
-avg_wt = wt * 1.0/NOP;  
-avg_tat = tat * 1.0/NOP;  
-printf("\n Average Turn Around Time: \t%f", avg_wt);  
-printf("\n Average Waiting Time: \t%f", avg_tat);  
-getch();  
-}  
+#include <stdio.h>
+
+int main() {
+    int i, sum = 0, gt[20], qt, n, bt[20], tt[20], wt[20], bt_cp[20], p[20], temp, count = 0, l = 0, k = 1, time[20] = {0};
+    float wt_avg = 0, tt_avg = 0;
+
+    printf("\nEnter the number of Processes(Max 20): ");
+    scanf("%d", &n);
+
+    printf("\nEnter the Burst Time of Each Process:\n");
+    for (int j = 0; j < n; j++) {
+        p[j] = j + 1;
+        printf("P%d : ", j + 1);
+        scanf("%d", &bt[j]);
+        bt_cp[j] = bt[j];
+    }
+
+    printf("\nEnter the Time Slice: ");
+    scanf("%d", &qt);
+
+    while (count != n) {
+        for (i = 0, count = 0; i < n; i++) {
+            if (bt_cp[i] == 0) {
+                count++;
+                continue;
+            }
+            if (bt_cp[i] > qt) {
+                bt_cp[i] -= qt;
+                temp = qt;
+            } else if (bt_cp[i] <= qt && bt_cp[i] > 0) {
+                temp = bt_cp[i];
+                bt_cp[i] = 0;
+            }
+            sum += temp;
+            tt[i] = sum;
+            gt[l++] = p[i];
+            time[k] = time[k - 1] + temp;
+            k++;
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        wt[i] = tt[i] - bt[i];
+        wt_avg += wt[i];
+        tt_avg += tt[i];
+    }
+
+    wt_avg /= n;
+    tt_avg /= n;
+
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (i = 0; i < n; i++)
+        printf("P%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], wt[i], tt[i]);
+
+    printf("\nAverage Waiting Time: %.2f ms", wt_avg);
+    printf("\nAverage Turnaround Time: %.2f ms\n", tt_avg);
+
+    printf("\n\n\t\t\t\tGantt Chart\n\n");
+    printf("------------------------------------------------------------------------\n");
+    for (i = 0; i < l; i++)
+        printf("|\tP%d\t|", gt[i]);
+    printf("\n------------------------------------------------------------------------\n");
+    for (i = 0; i < k; i++)
+        printf("%d\t   \t", time[i]);
+    printf("\n------------------------------------------------------------------------\n");
+}
