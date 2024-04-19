a09caf4d57e84e646444a5d60b04326117151d12
Santo cyriac <132189839+Tw0side@users.noreply.github.com>
2024-04-19 15:10:24 +0530

    Update Priorityschedle.c

--- Priorityschedle.c
+++ Priorityschedle.c
@@ -1,69 +1,60 @@
 #include <stdio.h>
- 
-void swap(int *a,int *b)
-{
-    int temp=*a;
-    *a=*b;
-    *b=temp;
-}
-int main()
-{
-    int n;
-    printf("Enter Number of Processes: ");
-    scanf("%d",&n);
 
-    int burst[n],priority[n],index[n];
-    for(int i=0;i<n;i++)
-    {
-        printf("Enter Burst Time and Priority Value for Process %d: ",i+1);
-        scanf("%d %d",&burst[i],&priority[i]);
-        index[i]=i+1;
+int main() {
+    int i, sum = 0, n, bt[20], tt[20], wt[20], p[20], priority[20], temp;
+    float wt_avg = 0, tt_avg = 0;
+
+    printf("\nEnter the number of Processes(Max 20): ");
+    scanf("%d", &n);
+
+    printf("\nEnter the Burst Time and Priority of Each Process:\n");
+    for (int j = 0; j < n; j++) {
+        p[j] = j + 1;
+        printf("P%d : ", j + 1);
+        scanf("%d %d", &bt[j], &priority[j]);
     }
-    for(int i=0;i<n;i++)
-    {
-        int temp=priority[i],m=i;
- 
-        for(int j=i;j<n;j++)
-        {
-            if(priority[j] > temp)
-            {
-                temp=priority[j];
-                m=j;
+
+    // Sorting processes based on priority
+    for (i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (priority[j] > priority[j + 1]) {
+                temp = priority[j];
+                priority[j] = priority[j + 1];
+                priority[j + 1] = temp;
+
+                temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+
+                temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
         }
- 
-        swap(&priority[i], &priority[m]);
-        swap(&burst[i], &burst[m]);
-        swap(&index[i],&index[m]);
     }
- 
-    int t=0;
- 
-    printf("Order of process Execution is\n");
-    for(int i=0;i<n;i++)
-    {
-        printf("P%d is executed from %d to %d\n",index[i],t,t+burst[i]);
-        t+=burst[i];
+
+    // Calculating turnaround time and waiting time
+    for (i = 0; i < n; i++) {
+        sum += bt[i];
+        tt[i] = sum;
+        wt[i] = tt[i] - bt[i];
+        wt_avg += wt[i];
+        tt_avg += tt[i];
     }
+
+    wt_avg /= n;
+    tt_avg /= n;
+
+    printf("\nProcess\tBurst Time\tPriority\tWaiting Time\tTurnaround Time\n");
+    for (i = 0; i < n; i++)
+        printf("P%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], priority[i], wt[i], tt[i]);
+
+    printf("\nAverage Waiting Time: %.2f ms", wt_avg);
+    printf("\nAverage Turnaround Time: %.2f ms\n", tt_avg);
+    printf("\t\t\t Gantt Chart \t\t\t\n");
+    printf("0");
+    for (i = 0; i < n; i++)
+        printf("  P%d  %d", p[i], tt[i]);
     printf("\n");
-    printf("Process Id\tBurst Time\tWait Time\n");
-    int wait_time=0;
-    int total_wait_time = 0;
-    for(int i=0;i<n;i++)
-    {
-        printf("P%d\t\t%d\t\t%d\n",index[i],burst[i],wait_time);
-        total_wait_time += wait_time;
-        wait_time += burst[i];
-    }
-    
-    float avg_wait_time = (float) total_wait_time / n;
-    printf("Average waiting time is %f\n", avg_wait_time);
-    
-    int total_Turn_Around = 0;
-    for(int i=0; i < n; i++){
-        total_Turn_Around += burst[i];
-    }
-    float avg_Turn_Around = (float) total_Turn_Around / n;
-    printf("Average TurnAround Time is %f",avg_Turn_Around);
     return 0;
 }
